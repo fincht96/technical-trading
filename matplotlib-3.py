6a2b44ec40d6657861d501dafa2a3c649512fccf
tom <thomas.finch@internationalgasdetectors.com>
2020-10-04 17:00:48 +0100

    Implemented 20 week ma calculator algorithm

--- matplotlib-3.py
+++ matplotlib-3.py
@@ -2,82 +2,78 @@ import matplotlib.pyplot as plt
 import numpy as np
 import yfinance as yf
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 
 def calcLastFriDate(date):
-    lastFriDate = 0
+ 
     dayNum = date.weekday()
-
-    if dayNum > 4:
-        dayNum = dayNum - 4
-        lastFriDate = date - timedelta(days=dayNum)
-
-    elif dayNum < 4:
-        dayNum = dayNum + 2
-        lastFriDate = date - timedelta(days=dayNum)
+    
+    
+    lastFriDate = date - relativedelta(days= dayNum + 3) 
 
     return lastFriDate
 
 
-def calc20WeekSma(date):
-    last20FridaysDates = []
-    last20FridaysClosingPrice = []
+def calc20WeekSma(date, stockData):
+    sum = 0
+    numReadingsLast20Weeks = 0
 
     # calcs date of prev friday
     lastFriDate = calcLastFriDate(date)
 
-    # generate dates of 20 fridays before
-    for i in range(0, 20):
-        last20FridaysDates.append(lastFriDate - timedelta(days=7 * i))
+    # iterate over last 20 values before this index in stockData
+    for i in range(0,20):
+        friDate = lastFriDate - relativedelta(weeks=(1*i))
+        price = stockData.loc[stockData.index == friDate.strftime("%Y-%m-%d"), 'Close']
+        if price.values.size > 0:
+            sum += price.values[0]
+            numReadingsLast20Weeks += 1
 
-    data = yf.download("AAPL", start="2019-09-26", end="2020-09-27")
-
-    print(last20FridaysDates)
-
-    sum = 0
-
-    for price in last20FridaysClosingPrice:
-        sum += price
-
-    return sum/20
+    return sum/numReadingsLast20Weeks
 
 
-data = yf.download("AAPL", start="2019-09-26", end="2020-09-27")
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
 
-x1 = []
-y1 = []
+if __name__ == '__main__':
 
+    ticker = "^FTSE"
 
-# 30 week sma
-x2 = []
-y2 = []
+    endDate = datetime.today()
+    startDate = datetime.today() - relativedelta(years=1)
 
+    stockData = yf.download(
+        ticker , start=(startDate - relativedelta(weeks=20)).strftime("%Y-%m-%d"), end=endDate.strftime("%Y-%m-%d"), progress=False)
 
-for item in data.T:
-    x1.append(item)
-    x2.append(item)
-
-
-for item in data.Close:
-    y1.append(item)
-
-
-print(calc20WeekSma(datetime.today()))
+ 
+    x1 = []
+    y1 = []
 
 
-for index, date in enumerate(x1):
-    # y2.append(calc20WeekSma(date))
-    # print(calc20WeekSma(date))
-    pass
+    mAX = []
+    mAY = []
+
+    for singleDate in daterange(startDate, endDate):
+
+        stock = stockData.loc[stockData.index == singleDate.strftime("%Y-%m-%d"), 'Close']
+
+        if stock.values.size > 0:
+            x1.append(singleDate)
+            y1.append(stock.values[0])
+            mAX.append(singleDate)
+            mAY.append(calc20WeekSma(singleDate, stockData))
+            
 
 
-plt.plot(x1, y1, label="line 1")
-# plt.plot(x2, y2, label="line 2")
+    plt.plot(x1, y1, label= ticker + " Stock Price")
+    plt.plot(mAX, mAY, label="20 Week MA")
 
+    plt.xlabel('Date')
+    plt.ylabel('Price ($)')
 
-plt.xlabel('Date')
-plt.ylabel('Price ($)')
+    plt.title(ticker + ' Stock')
 
-plt.title('Apple Stock ')
+    plt.show()
 
-# plt.show()
